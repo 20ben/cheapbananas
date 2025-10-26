f967b7ab8ca3769095f2b04c2b51b2bd35ce39eb
Matthew Kim <matthewkim1117@gmail.com>
2025-10-26 03:30:50 -0700

    letta-test2.py setup for 10 agent test

--- backend/letta-test2.py
+++ backend/letta-test2.py
@@ -10,24 +10,30 @@ load_dotenv()
 LETTA_API_TOKEN = os.environ.get('LETTA_API_TOKEN')
 if LETTA_API_TOKEN is None:
     raise ValueError("LETTA_API_TOKEN environment variable not set")
-MINION_ID = os.environ.get('MINION_ID')
-if MINION_ID is None:
-    raise ValueError("MINION_ID environment variable not set")
+
+
+MINION_ID1 = os.environ.get('MINION_ID1')
+if MINION_ID1 is None:
+    raise ValueError("MINION_ID1 environment variable not set")
+MINION_ID2 = os.environ.get('MINION_ID2')
+if MINION_ID2 is None:
+    raise ValueError("MINION_ID2 environment variable not set")
+
+
 READER_ID = os.environ.get('READER_ID')
 if READER_ID is None:
     raise ValueError("READER_ID environment variable not set")
 
 async def main():
     print("started!")
-    minionConnection = AsyncLettaMinion(LETTA_API_TOKEN, MINION_ID)
-    await minionConnection.connect_agent()
-    readerConnection = AsyncLettaReader(LETTA_API_TOKEN, READER_ID)
-    await readerConnection.connect_agent()
-    # print("passed minion connection")
-    await minionConnection.save_deals("Insomnia Cookies", "362 Kearny St")
+    minionConnection1 = AsyncLettaMinion(LETTA_API_TOKEN, MINION_ID1)
+    await minionConnection1.connect_agent()
+    minionConnection2 = AsyncLettaMinion(LETTA_API_TOKEN, MINION_ID2)
+    await minionConnection2.connect_agent()
 
-    deals = await readerConnection.read_deals("Insomnia Cookies", "362 Kearny St")
+    
 
-    print(deals)
+
+
 
 asyncio.run(main())
