c1b27c6a985d33fa812c2dc90765374ed004454b
Ben Hung <benhung@berkeley.edu>
2025-10-25 17:27:39 -0700

    More changes to backend

--- backend/server.py
+++ backend/server.py
@@ -9,44 +9,20 @@ x = datetime.datetime.now()
 
 # Initializing flask app
 app = Flask(__name__)
-CORS(app)  # enable cross-origin requests
-
-@app.route('/')
-def home():
-    return "Peanut butter jelly Peanut butter jelly Peanut butter jelly with a baseball bat"
-
-# test
-# Example data
-locations = [
-    {"id": 1, "name": "UC Berkeley", "lat": 37.8715, "lng": -122.2730},
-    {"id": 2, "name": "SF Downtown", "lat": 37.7749, "lng": -122.4194}
-]
-
-@app.route('/locations', methods=['GET'])
-def get_locations():
-    return jsonify(locations)
-
-@app.route('/add_location', methods=['POST'])
-def add_location():
-    data = request.get_json()
-    locations.append(data)
-    return jsonify({"message": "Location added", "data": data}), 201
-
-if __name__ == '__main__':
-    app.run(debug=True)
 
 
-
-# Route for seeing a data
+# Route for sending data
 @app.route('/data')
-def get_time():
+def output_deals():
+
+    # Letta agent call here...
 
     # Returning an api for showing in  reactjs
     return {
-        'Name':"geek", 
-        "Age":"22",
-        "Date":x, 
-        "programming":"python"
+        'name':"Insommia Cookies", 
+        "Deal":"Cookiewich",
+        "Description": "...", 
+        "Source":"https://instagram.com/post/postid"
         }
 
 # Route for receiving data from frontend... tbd on frontend side
@@ -54,9 +30,9 @@ def get_time():
 def submit_location():
     data = request.get_json()  # Get JSON data from request
     
-    # You can access specific fields like:
-    name = data.get('latitude')
-    email = data.get('longitude')
+    # Process the data (here we just print it)
+    print("Received data:", data)
+
     
     # Return a response
     return jsonify({
